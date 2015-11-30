334b2bfa775c86437cda5821446406d01c889a3e
Andreas Wojtok <fb4sb10006@FB4.FH>
2015-11-30 16:30:01 +0100

    Obsoletes Server Projekt entfernt

--- BeerBuddy-Server/UserService/src/main/java/de/fh/dortmund/beerbuddy/backend/entity/person/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package de.fh.dortmund.beerbuddy.backend.entity.person;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-/**
- * Created by seckinger on 14.09.2015.
- */
-public interface PersonRepository extends PagingAndSortingRepository<Person, String> {
-}
